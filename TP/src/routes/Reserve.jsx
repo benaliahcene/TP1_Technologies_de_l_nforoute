e3fed5a6889646b648be1199a39077968d56622a
ahcene benali <benalihassan845@gmail.com>
2023-10-10 14:18:05 -0400

    update

--- TP/src/routes/Reserve.jsx
+++ TP/src/routes/Reserve.jsx
@@ -1,29 +1,55 @@
 import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Container, Row, Col, Card } from "react-bootstrap";
+import { Container, Button, Nav, Row, Col, Card } from "react-bootstrap";
 import ReserveVol from "../components/ReserveVol";
 import ReserveRoom from "../components/ReserveRoom";
 import Menu from "../components/Menu";
-import Footer from "../components/Footer"
+import Footer from "../components/Footer";
+
 class MyComponent extends Component {
+    state = {
+        activeTab: 'vols'
+    };
+
+    setActiveTab = (tab) => {
+        this.setState({ activeTab: tab });
+    }
+
+    renderContent() {
+        switch (this.state.activeTab) {
+            case 'vols':
+                return <ReserveVol />;
+            case 'chambres':
+                return <ReserveRoom />;
+            default:
+                return <ReserveVol />;
+        }
+    }
+
     render() {
         return (
             <div>
-                <Menu/>
-                <Container style={{marginTop:"150px"}}>
-                    <Row>
-                        <Col>
-                            <Card>
-                                <Card.Header as="h2">Accueil</Card.Header>
-                                <Card.Body>
-                                    <ReserveVol />
-                                    <ReserveRoom />
-                                </Card.Body>
-                            </Card>
-                        </Col>
-                    </Row>
-                </Container>
-                <Footer/>
+                <Menu />
+                <Card>
+                    <Card.Header>
+                        <Nav variant="tabs" defaultActiveKey="vols">
+                            <Nav.Item>
+                                <Nav.Link eventKey="vols" onSelect={() => this.setActiveTab('vols')}>Vols</Nav.Link>
+                            </Nav.Item>
+                            <Nav.Item>
+                                <Nav.Link eventKey="chambres" onSelect={() => this.setActiveTab('chambres')}>Chambres</Nav.Link>
+                            </Nav.Item>
+                        </Nav>
+                    </Card.Header>
+                    <Card.Body>
+                        <Card.Title>Reserver maintenant !</Card.Title>
+                        <Card.Text>
+                            {this.renderContent()}
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+
+                <Footer />
             </div>
         );
     }
