96200ed9a97152e0ce105dd42ebd6891674d464f
rafik kasmi <rafik@dts-solution.com>
2023-10-12 00:13:04 +0100

    [add] added the flight service and installed axios

--- TP/src/routes/ReserveVol.jsx
+++ TP/src/routes/ReserveVol.jsx
@@ -4,14 +4,18 @@ import { Card } from "react-bootstrap";
 import ReserveVol from "../components/ReserveVol";
 import Menu from "../components/Menu";
 import Footer from "../components/Footer";
+import flightsService from "../services/flightsService";
 
 class MyComponent extends Component {
+  async componentDidMount() {}
   render() {
     return (
       <div>
-     
         <Card className="text-center">
-      <Card.Header> <h2>Des millions de vols pas chers. Une simple recherche.</h2></Card.Header>
+          <Card.Header>
+            {" "}
+            <h2>Des millions de vols pas chers. Une simple recherche.</h2>
+          </Card.Header>
           <Card.Body>
             <ReserveVol />
           </Card.Body>
